25592c95d2ea01aa20ebbe56cae09a17f516e173
Elizabeth Velasco <evelasco@thoughtworks.com>
2017-09-21 11:45:34 -0500

    adds API key

--- js/custom.js
+++ js/custom.js
@@ -28,7 +28,7 @@ function calculateCenter() {
 function loadGoogleMap(){
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' + 'callback=initialize';
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDfNrPwreCP4zkOOUyehuUYrbJFEhaK9IU?v=3.exp&' + 'callback=initialize';
     document.body.appendChild(script);
 }
 
