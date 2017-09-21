1d3721a9679ef630fb813588e3af4a9616db3f53
Elizabeth Velasco <evelasco@thoughtworks.com>
2017-09-20 20:13:32 -0500

    remove sensor from google API

--- js/custom.js
+++ js/custom.js
@@ -9,13 +9,13 @@ function initialize() {
       center: new google.maps.LatLng(40.7679619,-73.9800172),
       scrollwheel: false
     };
-  
+
     map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
 
     google.maps.event.addDomListener(map, 'idle', function() {
         calculateCenter();
     });
-  
+
     google.maps.event.addDomListener(window, 'resize', function() {
         map.setCenter(center);
     });
@@ -28,7 +28,7 @@ function calculateCenter() {
 function loadGoogleMap(){
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' + 'callback=initialize';
     document.body.appendChild(script);
 }
 
@@ -48,7 +48,7 @@ $(function(){
     type: 'image',
     gallery: {
       enabled: true
-    }    
+    }
   });
 
   loadGoogleMap();
@@ -57,9 +57,9 @@ $(function(){
 // isotope
 jQuery(document).ready(function($){
 
-  if ( $('.iso-box-wrapper').length > 0 ) { 
+  if ( $('.iso-box-wrapper').length > 0 ) {
 
-      var $container  = $('.iso-box-wrapper'), 
+      var $container  = $('.iso-box-wrapper'),
         $imgs     = $('.iso-box img');
 
       $container.imagesLoaded(function () {
@@ -80,18 +80,18 @@ jQuery(document).ready(function($){
 
           var $this = $(this), filterValue = $this.attr('data-filter');
 
-      $container.isotope({ 
+      $container.isotope({
         filter: filterValue,
-        animationOptions: { 
-            duration: 750, 
-            easing: 'linear', 
-            queue: false, 
-        }                
-      });             
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false,
+        }
+      });
 
-      // don't proceed if already selected 
-      if ( $this.hasClass('selected') ) { 
-        return false; 
+      // don't proceed if already selected
+      if ( $this.hasClass('selected') ) {
+        return false;
       }
 
       var filter_wrapper = $this.closest('.filter-wrapper');
@@ -99,7 +99,7 @@ jQuery(document).ready(function($){
       $this.addClass('selected');
 
         return false;
-      }); 
+      });
 
   }
 
